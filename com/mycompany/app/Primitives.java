008604bb36f795caeaea58928144112764360161
Abdulaziz <alobodi1@hotmail.com>
2017-10-26 17:25:26 -0400

    addDouble

--- com/mycompany/app/Primitives.java
+++ com/mycompany/app/Primitives.java
@@ -4,9 +4,17 @@ package com.mycompany.app;
 public class Primitives 
 {
    
-    public static int add (int x, int y){
+    public static int addInt (int x, int y){
         int res;
 		res = Math.addExact(x, y);
+        return res;
+    }
+    public static double addDouble (double x, double y){
+        double res;
+        res = Math.addExact(x, y);
+        if(x > 5){
+            x++;
+        }
         return res;
     }
 }
