2aef602fcd638055ff02b0b1fe812db7ac622dde
Abdulaziz <alobodi1@hotmail.com>
2017-10-27 18:25:28 -0400

    All changes

--- com/mycompany/app/Primitives.java
+++ com/mycompany/app/Primitives.java
@@ -5,50 +5,49 @@ public class Primitives
 {
    public static int number = 1;
     public static int addInt (int x, int y){
-        int results;
-        results = 
+        int result;
+        result = 
         Math.addExact(x, x);
-        ++results;
-        return results;
+        ++result;
+        return result;
     }
     public static double addDouble (double x, double y){
-        double results;
-        results = x+y;
-        return results;
+        double result;
+        result = x+y;
+        return result;
     }
     public static float addFloat (float x, float y){
-		float resultsf;
-		resultsf = x+y;
-		resultsf = Math.abs(resultsf);
-        return resultsf;
+		float result;
+		result = x+y;
+		result = Math.abs(result);
+        return result;
     }
     public static long addlong (long x, long y){
-		long results;
-		results = x+y;
-		results = Math.abs(results);
-        return results;
+		long result;
+		result = x+y;
+		result = Math.abs(result);
+        return result;
     }
     public static int addshort (short x, short y){
-		int results;
-		results = Math.addExact(x,y);
+		int result;
+		result = Math.addExact(x,y);
 		x++;
-        return results;
+        return result;
     }
     public static char addchar (char x, char y){
-		String results;
-		int i = Math.addExact(x, y);
-		results = String.valueOf(y);
-        return results.toCharArray()[0];
+		char result;
+		result = String.valueOf(y).toCharArray()[0];
+        return result;
     }
     public static byte addByte (byte x, byte y){
-		byte results;
+		byte result;
 		byte [] i =  new byte[10];
-		results = i[0];
-        return results;
+		result = i[0];
+        return result;
     }
     public static boolean addboolean (boolean x, boolean y){
-		boolean results;
-		results = Boolean.logicalOr(x, y);
-        return results;
+		boolean result;
+		result = Boolean.logicalOr(x, y);
+        return result;
 	}
 }
