e48b2b11b64560eebe986cc777dda8954c6f512f
Abdulaziz <alobodi1@hotmail.com>
2017-10-27 11:16:02 -0400

    addint changes

--- com/mycompany/app/Primitives.java
+++ com/mycompany/app/Primitives.java
@@ -3,10 +3,11 @@ package com.mycompany.app;
 
 public class Primitives 
 {
-   
+   public static number = 1;
     public static int addInt (int x, int y){
         int res;
-        res = Math.addExact(x, y);
+        res = 
+        Math.addExact(x, y);
         res++;
         return res;
     }
