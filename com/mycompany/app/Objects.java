4d7c98a31942a59fe76a0d1170cfdf23539699b6
Abdulaziz <alobodi1@hotmail.com>
2017-11-11 12:30:36 -0500

    String as an object

--- com/mycompany/app/Objects.java
+++ com/mycompany/app/Objects.java
@@ -1,15 +1,16 @@
+package com.mycompany.app;
 
 public class Objects {
 
     public static String createStringFrom(String x) {
-		String s = x;
-        return s;
+		String str = x;
+        return str;
     }
     public static boolean equalsStrings(String x, String y) {
-        Boolean s =  x == y && x.equals(y);
-        return s;
+        Boolean str =  x == y && x.equals(y);
+        return str;
     }
     public static String Factory(){
-          return new String();
+           return new String();
     }
 }   
