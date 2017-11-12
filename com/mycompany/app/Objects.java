ec46b97db8c8388b8bdbf541acb1c87e2175a57d
Abdulaziz <alobodi1@hotmail.com>
2017-11-12 11:30:46 -0500

    string

--- com/mycompany/app/Objects.java
+++ com/mycompany/app/Objects.java
@@ -3,12 +3,12 @@ package com.mycompany.app;
 public class Objects {
 
     public static String createStringFrom(String x) {
-		String str = x;
-        return str;
+		String string = x;
+        return string;
     }
     public static String equalsStrings(String x, String y) {
-        String str =  x.concat(y) + "!";
-        return str;
+        String string =  x.concat(y) + "!";
+        return string;
     }
     public static String Factory(){
          return new String();
