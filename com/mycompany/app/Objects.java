dfec3371cbdf3c4cc5318b860753bb4d542bf7ee
Abdulaziz <alobodi1@hotmail.com>
2017-11-12 11:28:01 -0500

    string

--- com/mycompany/app/Objects.java
+++ com/mycompany/app/Objects.java
@@ -3,14 +3,14 @@ package com.mycompany.app;
 public class Objects {
 
     public static String createStringFrom(String x) {
-		String str = x;
-        return str;
+		String string = x;
+        return string;
     }
-    public static boolean equalsStrings(String x, String y) {
-        Boolean str =  x == y && x.equals(y);
-        return str;
+    public static String equalsStrings(String x, String y) {
+        String string =  x == y && x.equals(y);
+        return string;
     }
     public static String Factory(){
-           return new String();
+        return new String();
     }
 }   
